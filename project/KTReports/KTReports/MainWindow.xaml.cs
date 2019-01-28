67044b63b2c30fa7bcd5f1038bd0f19905516c71
Tony Bhangal <bhangat@LabImage2016-17>
2019-01-28 13:15:28 -0800

    open files and report options

--- project/KTReports/KTReports/MainWindow.xaml.cs
+++ project/KTReports/KTReports/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace KTReports
 {
@@ -28,6 +30,27 @@ namespace KTReports
         private void Close_Clicked(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+        [STAThread]
+        private void Import_File(object sender, RoutedEventArgs e)
+        {
+            String path;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm|CSV files|*.csv";
+            openFileDialog.Title = "Select a file to import";
+            openFileDialog.ShowDialog();
+            path = openFileDialog.FileName;
+        }
+        private void Report_Options(object sender, RoutedEventArgs e)
+        {
+            rOptions.IsEnabled = true;
+            rOptions.Visibility = Visibility.Visible;
+        }
+
+        private void Generate_Report(object sender, RoutedEventArgs e)
+        {
+            rOptions.IsEnabled = false;
+            rOptions.Visibility = Visibility.Hidden;
         }
     }
 }
