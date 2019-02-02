edcddfef567c00702186fa720821df66b352b3c8
Tony Bhangal <bhangat@LabImage2016-17>
2019-02-01 17:09:02 -0800

    sprint 1

--- project/KTReports/KTReports/MainWindow.xaml.cs
+++ project/KTReports/KTReports/MainWindow.xaml.cs
@@ -32,15 +32,100 @@ namespace KTReports
             this.Close();
         }
         [STAThread]
+
         private void Import_File(object sender, RoutedEventArgs e)
+        {
+            string fileName = "";
+            OpenFileDialog fileDia = new OpenFileDialog();
+            fileDia.Filter = "Excel/CSV Files|*.xls;*.xlsx;*.xlsm;*.csv";
+            fileDia.Title = "Select a file to import";
+            fileDia.FilterIndex = 2;
+            fileDia.ShowDialog();
+            //fileDia.RestoreDirectory = true;
+            fileName = fileDia.FileName;
+
+            if (fileName.Length > 2)
+            {
+
+                Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(fileName);
+                int sheetCount = xlWorkbook.Sheets.Count;
+                //Loop through each sheet in the file
+                for (int sheetNum = 1; sheetNum <= sheetCount; sheetNum++)
+                {
+                    file.IsEnabled = true;
+                    file.Visibility = Visibility.Visible;
+                    Microsoft.Office.Interop.Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[sheetNum];
+                    Microsoft.Office.Interop.Excel.Range xlRange = xlWorksheet.UsedRange;
+
+                    int rowCount = xlRange.Rows.Count;
+                    int colCount = xlRange.Columns.Count;
+
+                    //dataGridView1.ColumnCount = colCount;
+                    //dataGridView1.RowCount = rowCount;
+
+                    for (int i = 1; i <= rowCount; i++)
+                    {
+                        for (int j = 1; j <= colCount; j++)
+                        {
+                            //write to a grid
+                            if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                            {
+                                //string text = System.IO.File.ReadAllText(@fileName);
+                                textFile.AppendText(xlRange.Cells[i, j].Value2.ToString());
+                                //dataGridView1.Rows[i - 1].Cells[j - 1].Value = xlRange.Cells[i, j].Value2.ToString();
+                            }
+
+                            /*
+                            if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                            {
+                                //append to a string?
+                            }
+                            */
+        }
+        textFile.AppendText("\n");
+        //send list to Database?
+        }
+        /*
+        //cleanup  
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        //release com objects to fully kill excel process from running in the background
+        X.ReleaseComObject(xlRange);
+        X.ReleaseComObject(xlWorksheet);
+        //close and release  
+        xlWorkbook.Close();
+        X.ReleaseComObject(xlWorkbook);
+        //quit and release  
+        xlApp.Quit();
+        X.ReleaseComObject(xlApp);
+        */
+        }
+        }
+        }
+
+       /* private void Import_File(object sender, RoutedEventArgs e)
         {
             String path;
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm|CSV files|*.csv";
+            openFileDialog.Filter = "Excel/CSV Files|*.xls;*.xlsx;*.xlsm;*.csv";
             openFileDialog.Title = "Select a file to import";
             openFileDialog.ShowDialog();
             path = openFileDialog.FileName;
-        }
+
+            if(path.Length > 2)
+            {
+                file.IsEnabled = true;
+                file.Visibility = Visibility.Visible;
+                string text = System.IO.File.ReadAllText(@path);
+                textFile.AppendText(text);
+            }
+
+            //string text = System.IO.File.ReadAllText(@path);
+            //System.Console.WriteLine("Contents of file = {0}", text);
+        }*/
+        
+
         private void Report_Options(object sender, RoutedEventArgs e)
         {
             rOptions.IsEnabled = true;
@@ -51,6 +136,12 @@ namespace KTReports
         {
             rOptions.IsEnabled = false;
             rOptions.Visibility = Visibility.Hidden;
+        }
+
+        private void Done_File(object sender, RoutedEventArgs e)
+        {
+            file.IsEnabled = false;
+            file.Visibility = Visibility.Hidden;
         }
     }
 }
